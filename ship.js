25b5a803435226072c711c43ff587762f620c23b
Lakshya Tyagi <lakshyatyagi2005@gmail.com>
2024-07-11 16:00:56 +0530

    add:basic config,test and setup files

---
+++ ship.js
@@ -0,0 +1,27 @@
+class ship {
+  constructor() {
+    this.carrierHits = 0;
+    this.battleShipHits = 0;
+    this.destroyerHits = 0;
+    this.submarineHits = 0;
+    this.patrolBoatHits = 0;
+    return;
+  }
+  carrier() {
+    return {};
+  }
+  battleShip() {
+    return {};
+  }
+  destroyer() {
+    return {};
+  }
+  submarine() {
+    return {};
+  }
+  patrolBoat() {
+    return {};
+  }
+}
+
+export default ship;
